5fc8aeab85512e0d488d660dfb83399c36576a33
Amal Mohammed <157481527+amolllat@users.noreply.github.com>
2024-02-28 13:28:56 +0300

    done

---
+++ index.js
@@ -0,0 +1,14 @@
+let divide = function(a,b) {
+    return a/b ;
+}
+console.log(2000/100);
+
+let square =(number) => number * number;
+let x =2 ;
+console.log(square(x));
+
+let add = (a,b) => a + b ;
+let a = 3 ;
+let b = 4 ;
+console.log(add(a,b));
+
